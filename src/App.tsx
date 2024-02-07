f701dddaf8e630fe8e8994bd69636e4945a1810f
Daniil Borinskiy <dboryn16@gmail.com>
2024-02-07 18:56:33 +0200

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,10 @@ export const App: React.FC = () => (
   <>
     <div />
     <h1>Vite + React</h1>
-    <div className='card'>
+    <div className='card py-12'>
       <p>
         Edit <code>src/App.tsx</code> and save to test HMR
       </p>
     </div>
-    <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
   </>
 );
