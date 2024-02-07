e0ada4d7dd8ce1d092f2c27d8eea26788d55a670
Daniil Borinskiy <dboryn16@gmail.com>
2024-02-07 18:48:08 +0200

    init

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import legacy from '@vitejs/plugin-legacy';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+  },
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+  ],
+});
